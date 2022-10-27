42da34591698dbcb9992199812d7b65a5d8f8425
Stevewu1234 <plgawu@163.com>
2022-10-27 16:48:15 +0800

    add merkle distribution generation scripts

---
+++ src/generateMerkleTree.ts
@@ -0,0 +1,23 @@
+import {
+  merkletree_generator,
+  MerkleDistributorInfo,
+} from "merkle-distributor-array";
+import fs from "fs";
+import path from "path";
+
+const generateMerkleTree = () => {
+  const originalFile = path.join(__dirname, "./originalData/example.json");
+
+  const generatePath = path.join(__dirname, "../chunks");
+
+  const merkle: MerkleDistributorInfo = merkletree_generator(
+    originalFile,
+    generatePath
+  );
+
+  console.log("generated Merkle Root: ", merkle.merkleRoot);
+};
+
+(() => {
+  generateMerkleTree();
+})();
